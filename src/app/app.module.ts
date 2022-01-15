7fab1dc08a86415c327533ceaea916db305a08e8
madden20202 <business.mocksock@gmail.com>
2022-01-14 16:13:04 -0800

    add heroes-list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { SimpleRouterComponent } from './simple-router/simple-router.component';
 import { CrisisListComponent } from './simple-router/crisis-list/crisis-list.component';
+import { HeroesListComponent } from './simple-router/heroes-list/heroes-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SimpleRouterComponent,
-    CrisisListComponent
+    CrisisListComponent,
+    HeroesListComponent
   ],
   imports: [
     BrowserModule
