0cecc0e931bac7ada4b89d86d716f067c694399f
madden20202 <business.mocksock@gmail.com>
2022-01-15 10:48:15 -0800

    added page not found component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { SimpleRouterComponent } from './simple-router/simple-router.component';
 import { CrisisListComponent } from './simple-router/crisis-list/crisis-list.component';
 import { HeroesListComponent } from './simple-router/heroes-list/heroes-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SimpleRouterComponent,
     CrisisListComponent,
-    HeroesListComponent
+    HeroesListComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
