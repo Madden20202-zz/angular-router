ec8b08d92992e12a989a17b039857dee7031ded1
madden20202 <business.mocksock@gmail.com>
2022-01-18 15:55:54 -0800

    removing imports of simple router work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { SimpleRouterComponent } from './simple-router/simple-router.component';
 import { CrisisListComponent } from './simple-router/crisis-list/crisis-list.component';
 import { HeroesListComponent } from './simple-router/heroes-list/heroes-list.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { AppRoutingModule } from './app-routing.module';
-import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SimpleRouterComponent,
     CrisisListComponent,
     HeroesListComponent,
     PageNotFoundComponent
