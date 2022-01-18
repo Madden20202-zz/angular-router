1e244f053a50fb1f08bef8e6e4e031ee5db94b6e
madden20202 <business.mocksock@gmail.com>
2022-01-18 15:41:54 -0800

    add imports and paths, and routerModule for root

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,24 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes} from '@angular/router'
 
+import { HeroesListComponent } from './simple-router/heroes-list/heroes-list.component';
+import { CrisisListComponent } from './simple-router/crisis-list/crisis-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
+const appRoutes: Routes = [
+  {path:'crisis-center', component: CrisisListComponent},
+  {path:'heroes', component: HeroesListComponent},
+  {path: '', redirectTo: '/heroes', pathMatch: 'full'},
+  { path: '**', component: PageNotFoundComponent}
+];
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true} //used for debugging
+    )
+  ],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
