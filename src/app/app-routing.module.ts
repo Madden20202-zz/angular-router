3d740d4d675b2b598153e18e77e626f002e964be
madden20202 <business.mocksock@gmail.com>
2022-01-18 16:22:40 -0800

    fix some spacing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes} from '@angular/router'
 
-import { HeroesListComponent } from './simple-router/heroes-list/heroes-list.component';
 import { CrisisListComponent } from './simple-router/crisis-list/crisis-list.component';
+import { HeroesListComponent } from './simple-router/heroes-list/heroes-list.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const appRoutes: Routes = [
-  {path:'crisis-center', component: CrisisListComponent},
-  {path:'heroes', component: HeroesListComponent},
-  {path: '', redirectTo: '/heroes', pathMatch: 'full'},
-  { path: '**', component: PageNotFoundComponent}
+  { path: 'crisis-center', component: CrisisListComponent },
+  { path: 'heroes', component: HeroesListComponent },
+  { path: '', redirectTo: '/heroes', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
